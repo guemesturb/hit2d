fe63773dca42ace175a548955304d9b149be3286
guemesturb <guemes.turb@gmail.com>
2022-01-24 13:55:01 +0100

    Fix README

--- Makefile
+++ Makefile
@@ -2,6 +2,10 @@ CC = g++
 CPU_SOURCES = $(wildcard src/*.cpp)
 CPU_OBJECTS = $(CPU_SOURCES:.cpp=.o)
 
+NX:= $(shell python readconf.py NX)
+NY:= $(shell python readconf.py NY)
+SIZE = -DNX=${NX} -DNY=${NY}
+
 all: $(CPU_OBJECTS)
 	$(CC) -o channel.bin $(CPU_OBJECTS)
 
--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,20 @@
 #include <iostream>
 
+using namespace std;
+
 int main(int argc, char **argv) {
-    std::wcout << "Hello world!";
+    
+    // Define input parameters
+    
+    int LX = 2;
+    int LY = 2;
+
+    // Main execution logic
+
+    // End program
+
+    cout << "Simulation is finished\n";
+    
     return 0;
 }
 
